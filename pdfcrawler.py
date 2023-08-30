3bad3ac440811046db346fcabf59dea3c4142e26
James <101947821+pmjamez@users.noreply.github.com>
2023-08-30 14:11:17 -0700

    merge

--- pdfcrawler.py
+++ pdfcrawler.py
@@ -1,8 +1,33 @@
-<<<<<<< HEAD
-import selenium 
+import selenium
+from selenium import webdriver
+import requests
 import os
 
+driver = webdriver.Chrome()  
 
-=======
-import selenium 
->>>>>>> a51c7e8ac513668a8a044f976847ef193819cc93
+company_names = ["AMD", "TSMC", "Microchip"]
+
+pdf_folder = "pdf_reports"
+os.makedirs(pdf_folder, exist_ok=True)
+
+
+for company_name in company_names:
+    search_query = f"{company_name} esg report 2023 filetype:pdf"
+    
+    driver.get(f"https://www.google.com/search?q={search_query}")
+    
+    search_results = driver.find_elements_by_css_selector("a")
+    
+    for result in search_results:
+        link_url = result.get_attribute("href")
+        if link_url and link_url.endswith(".pdf"):
+            
+            response = requests.get(link_url)
+            pdf_path = os.path.join(pdf_folder, f"{company_name}_esg_report_2023.pdf")
+            with open(pdf_path, "wb") as pdf_file:
+                pdf_file.write(response.content)
+            break 
+        
+driver.quit()
+
+
