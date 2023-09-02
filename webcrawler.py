e14c20360e2e4e874c33774ec3207cc120e97ec3
James <101947821+pmjamez@users.noreply.github.com>
2023-09-01 22:04:21 -0700

    webcrawler changes, added validation steps and functionized code

--- webcrawler.py
+++ webcrawler.py
@@ -1,8 +1,8 @@
-import selenium
 from selenium import webdriver
 import os
 import pip._vendor.requests
 import time
+import fitz
 
 ##############
 #start of folder creation
@@ -20,46 +20,116 @@ if not os.path.exists(folder_path):
 else:
     print("Folder already exists, writing PDF files now")
 
+def is_pdf_corrupted(pdf_filepath):
+     try:
+        pdf_document = fitz.open(pdf_filepath)
+        time.sleep(2)
+        num_pages = pdf_document.page_count
+        return num_pages > 0
+     except Exception as e:
+        return False
+    
+
 ###########
 # start of web scraper
 
 browser = webdriver.Chrome()
 
+def searcher(company_name):
+
+    search_query = f"{company_name} sustainability report 2023 filetype:pdf"
+    browser.get(f"https://www.google.com/search?q={search_query}")
+
+    try:
+         first_result = browser.find_element("css selector", "div.tF2Cxc")
+         pdf_link = first_result.find_element("css selector", "a").get_attribute("href")
+         print("URL of " + company_name +"'s link:")
+    except Exception as e:
+         print("Error:", e)
+
+    time.sleep(3)
+    print("0")
+    pdf_response = pip._vendor.requests.get(pdf_link)
+    print("1")
+    time.sleep(3)
+    pdf_content = pdf_response.content
+    print("12")
+    pdf_filename = f"{company_name}_ESG_report.pdf"
+    print("123")
+    pdf_filepath = os.path.join(folder_path, pdf_filename)
+    print("1234")
+
+    time.sleep(1)
+
+    with open(pdf_filepath, "wb") as pdf_file:
+            print("creating file")
+            
+            pdf_file.write(pdf_content)
+    
+    return pdf_filepath
+
+def searcherpotion2(company_name):
+
+    search_query = f"{company_name} Corporate Responsibility report 2023 filetype:pdf"
+    browser.get(f"https://www.google.com/search?q={search_query}")
+
+    try:
+         first_result = browser.find_element("css selector", "div.tF2Cxc")
+         pdf_link = first_result.find_element("css selector", "a").get_attribute("href")
+         print("URL of " + company_name +"'s link:")
+    except Exception as e:
+         print("Error:", e)
+
+    time.sleep(3)
+
+    pdf_response = pip._vendor.requests.get(pdf_link)
+    time.sleep(3)
+    pdf_content = pdf_response.content
+    pdf_filename = f"{company_name}_ESG_report.pdf"
+    pdf_filepath = os.path.join(folder_path, pdf_filename)
+
+    time.sleep(1)
+
+    with open(pdf_filepath, "wb") as pdf_file:
+            print("creating file")
+            
+            pdf_file.write(pdf_content)
+    
+    return pdf_filepath
+    
 company_names = ["AMD", "TSMC", "Microchip", "Micron"]
 # list of company names here
 
 for company_name in company_names:
 
-    search_query = f"{company_name} sustainability report 2023 filetype:pdf"
+    pdf_filepath1 = searcher(company_name)
 
-    browser.get(f"https://www.google.com/search?q={search_query}")
+    if is_pdf_corrupted(pdf_filepath1):
+        print("The PDF is not corrupted.")
+    else:
+        print("The PDF is corrupted.")
+        os.remove(pdf_filepath1)
+        print("File has been deleted")
+        continue
 
-    time.sleep(1)
+    pdf_document = fitz.open(pdf_filepath1)
+    num_pages = pdf_document.page_count
 
-    try:
-         first_result = browser.find_element("css selector", "div.tF2Cxc")
-         pdf_link = first_result.find_element("css selector", "a").get_attribute("href")
-         print("URL of " + company_name +"'s link:", pdf_link)
-    except Exception as e:
-         print("Error:", e)
+    if num_pages < 10:
+        os.remove(pdf_filepath1)
+        print("File too small")
+        searcherpotion2(company_name)
 
-    pdf_response = pip._vendor.requests.get(pdf_link)
-    pdf_content = pdf_response.content
-    pdf_filename = f"{company_name}_sustainability_report.pdf"
-    pdf_filepath = os.path.join(folder_path, pdf_filename)
+    # time.sleep(5)
+    # page_reader = PyPDF2.PdfFileReader(pdf_file)
+    # num_pages = page_reader.getNumPages()
 
-    with open(pdf_filepath, "wb") as pdf_file:
-            pdf_file.write(pdf_content)
-    print(f"PDF saved: {pdf_filepath}")
+    
 
-    time.sleep(1)
+    print(f"PDF saved and # of pages: {num_pages}")
 
-    # response = pip._vendor.requests.get(pdf_link)
-    # pdf_content = response.content
 
-    # with open(folder_name, "wb") as pdf_file:
-    #  pdf_file.write(pdf_content)
-    #  print(f"PDF saved: {pdf_file}")
+   
 browser.quit()
 
 
